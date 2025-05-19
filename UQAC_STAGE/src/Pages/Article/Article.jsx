0fba74ed7e57c0ef8d185514a2485c638d6813cb
alexisChab <alexis.chab73@gmail.com>
2025-05-18 21:12:15 -0400

    ajout du bouton pour ajouter un article

--- UQAC_STAGE/src/Pages/Article/Article.jsx
+++ UQAC_STAGE/src/Pages/Article/Article.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useArticleWithTags } from "../../hooks/useArticleWithTags";
+import { db } from "../../config/Firebase";
+import { addDoc, collection, Timestamp, doc } from "firebase/firestore";
+import { useAuth } from "../../contexts/AuthContext";
 import "./Article.css";
 import LikeButton from "../../Components/UI/Button/LikeButton";
 
 export default function Article() {
   const { id } = useParams();
   const { article, tags, loading, error, isEmpty } = useArticleWithTags(id);
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    const addToHistorique = async () => {
+      if (!currentUser || !id) return;
+      try {
+        await addDoc(collection(db, "historique"), {
+          contenuId: doc(db, "Publications", id),
+          utilisateurId: currentUser.uid,
+          date: Timestamp.now(),
+        });
+      } catch (err) {
+        console.error("Erreur lors de l'enregistrement dans l'historique:", err);
+      }
+    };
+
+    addToHistorique();
+  }, [currentUser, id]);
 
   const formatDate = (firestoreDate) => {
     if (!firestoreDate) return "";
